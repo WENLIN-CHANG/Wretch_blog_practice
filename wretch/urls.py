10c95802532e5ec029818f186a282aa3144b95f7
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-08-04 15:42:01 +0800

    重構：移動views到views.py

--- wretch/urls.py
+++ wretch/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
+from .views import about, home
 
-
-def home(request):
-    return render(request, "home.html")
-
-def about(request):
-    return render(request, "about.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
