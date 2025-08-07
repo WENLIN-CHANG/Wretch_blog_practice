dd3c125db4dd6ca8067620a56d44d32ea8df2e59
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-08-07 11:31:59 +0800

    加admin,model

--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,9 @@ class Article(models.Model):
     title = models.CharField(max_length=200, null=False)
     content = models.TextField(null=True)
 
+    def __str__(self):
+        return self.title
+
 # 1. 編輯 models.py
 # 2. 執行 uv run python manage.py makemigrations APP_NAME
 # 3. 檢查 uv run python manage.py sqlmigrate APP_NAME 編號
