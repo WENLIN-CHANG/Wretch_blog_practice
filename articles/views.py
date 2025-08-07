826322817994dedbfa202e7b001512b9e26e64ac
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-08-07 14:13:50 +0800

    增加訊息提示

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Article
+from django.contrib import messages
 
 def index(request):
     if request.POST:
@@ -8,6 +9,7 @@ def index(request):
         content = request.POST["content"]
         # 寫入 table
         Article.objects.create(title=title, content=content)
+        messages.success(request, "新增成功")
         return redirect("articles:index")
     else:
         articles = Article.objects.all().order_by("-id")
@@ -25,10 +27,14 @@ def detail(request, id):
             article.content = request.POST["content"]
             article.save()
 
+            messages.success(request, "更新成功")
             return redirect("articles:detail", article.id)
 
         if request.POST["_method"] == "delete":
-            pass
+            article.delete()
+            
+            messages.warning(request, "刪除成功")
+            return redirect("articles:index")
     else:
         return render(request, "articles/detail.html", {"article": article})
 
