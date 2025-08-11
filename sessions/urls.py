e563b8acf0b71764841c9cba5ae99b751202838c
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-08-11 23:27:12 +0800

    修復小bug

--- sessions/urls.py
+++ sessions/urls.py
@@ -5,7 +5,6 @@ app_name = "sessions"
 
 urlpatterns = [
     path("", views.new, name="new"),
-    path("new/", views.new, name="new"),
     path("create/", views.create, name="create"),
     path("logout/", views.logout, name="logout"),
 ]
